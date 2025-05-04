b0b18e28b62f7f3475c7df83e1dc6f746b501617
jitesh <jitesh02sep@gmail.com>
2025-05-05 00:22:52 +0530

    fixed the build issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
-import Clients from "@/components/Clients";
+// app/page.tsx
+
+import dynamic from "next/dynamic";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
+
+// Dynamically import components that likely use document/window
+const Hero = dynamic(() => import("@/components/Hero"), { ssr: false });
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
+const Clients = dynamic(() => import("@/components/Clients"), { ssr: false });
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-
-    <FloatingNav navItems={navItems} />
-    <Hero />
-   <Grid />
-   <Experience />
-   <Clients />
-   <Footer />
-    </div>
-   </main>
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <Experience />
+        <Clients />
+        <Footer />
+      </div>
+    </main>
   );
 }
